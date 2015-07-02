a010377c27033c59ab8325e8c3b34567840da0d4
Memnarch <Sebal007@freenet.de>
2015-07-02 20:16:39 +0200

    implemented TDNEnvironmentOptions and TDNEnvironmentOptionsService for handling platform specific enviroment settings which is lacking from ToolsAPi

---
+++ DN.ToolsApi.Extension.pas
@@ -0,0 +1,280 @@
+unit DN.ToolsApi.Extension;
+
+interface
+
+uses
+  Classes,
+  Windows,
+  Registry,
+  Generics.Collections,
+  DN.Compiler.Intf,
+  DN.ToolsApi.Extension.Intf;
+
+type
+  TDNEnvironmentOptions = class(TInterfacedObject, IDNEnvironmentOptions)
+  private
+    FPlatform: TDNCompilerPlatform;
+    FRegstryKey: string;
+    FRegistry: TRegistry;
+    FUpdateLevel: Integer;
+    function GetPlatform: TDNCompilerPlatform;
+    function Open(): Boolean;
+    procedure Close();
+    function ReadString(const AName: string): string;
+    procedure WriteString(const AName, AValue: string);
+    function GetBPLOutput: string;
+    function GetBrowsingPath: string;
+    function GetDCPOutput: string;
+    function GetSearchPath: string;
+    procedure SetBPLOutput(const Value: string);
+    procedure SetBrowsingPath(const Value: string);
+    procedure SetDCPOutput(const Value: string);
+    procedure SetSearchPath(const Value: string);
+    procedure Changed();
+  public
+    constructor Create(APlatform: TDNCompilerPlatform; const ARegistryKey: string);
+    destructor Detsroy();
+    procedure BeginUpdate();
+    procedure EndUpdate();
+    property Platform: TDNCompilerPlatform read GetPlatform;
+    property BrowingPath: string read GetBrowsingPath write SetBrowsingPath;
+    property SearchPath: string read GetSearchPath write SetSearchPath;
+    property BPLOutput: string read GetBPLOutput write SetBPLOutput;
+    property DCPOutput: string read GetDCPOutput write SetDCPOutput;
+  end;
+
+  TDNEnvironmentOptionsService = class(TInterfacedObject, IDNEnvironmentOptionsService)
+  private
+    FSupportedPlatforms: TDNCompilerPlatforms;
+    FOptions: TList<IDNEnvironmentOptions>;
+    function GetOptions(
+      const APlatform: TDNCompilerPlatform): IDNEnvironmentOptions;
+    function GetSupportedPlatforms: TDNCompilerPlatforms;
+    procedure LoadPlatforms();
+  public
+    constructor Create();
+    destructor Destroy(); override;
+    property Options[const APlatform: TDNCompilerPlatform]: IDNEnvironmentOptions read GetOptions;
+    property SupportedPlatforms: TDNCompilerPlatforms read GetSupportedPlatforms;
+  end;
+
+implementation
+
+uses
+  IOUtils,
+  ToolsApi,
+  DN.ToolsApi;
+
+const
+  CLibraryKey = 'Library';
+  CWin32Key = 'Win32';
+  CWin64Key = 'Win64';
+
+{ TDNEnvironmentOptionsService }
+
+constructor TDNEnvironmentOptionsService.Create;
+begin
+  inherited;
+  FOptions := TList<IDNEnvironmentOptions>.Create();
+  LoadPlatforms();
+end;
+
+destructor TDNEnvironmentOptionsService.Destroy;
+begin
+  FOptions.Free;
+  inherited;
+end;
+
+function TDNEnvironmentOptionsService.GetOptions(
+  const APlatform: TDNCompilerPlatform): IDNEnvironmentOptions;
+var
+  LOption: IDNEnvironmentOptions;
+begin
+  Result := nil;
+  for LOption in FOptions do
+  begin
+    if LOption.Platform = APlatform then
+      Exit(LOption);
+  end;
+end;
+
+function TDNEnvironmentOptionsService.GetSupportedPlatforms: TDNCompilerPlatforms;
+begin
+  Result := FSupportedPlatforms;
+end;
+
+procedure TDNEnvironmentOptionsService.LoadPlatforms;
+var
+  LService: IOTAServices;
+  LReg: TRegistry;
+  LBase, LLibraryKey, LPlatformKey: string;
+  LNames: TStringList;
+begin
+  LService := BorlandIDEservices as IOTAServices;
+  LBase := LService.GetBaseRegistryKey();
+  LReg := TRegistry.Create();
+  LNames := TStringList.Create();
+  try
+    LReg.RootKey := HKEY_CURRENT_USER;
+    LLibraryKey := TPath.Combine(LBase ,CLibraryKey);
+    if LReg.OpenKey(LLibraryKey, False) then
+    begin
+      LReg.GetValueNames(LNames);
+      //we are on a Win32-Only Delphi
+      if LNames.Count > 0 then
+      begin
+        FOptions.Add(TDNEnvironmentOptions.Create(cpWin32, LLibraryKey) as IDNEnvironmentOptions);
+        FSupportedPlatforms := [cpWin32];
+      end
+      else
+      begin
+        //we are on a multiplatform Delphi
+        FSupportedPlatforms := [];
+        if LReg.KeyExists(CWin32Key) then
+        begin
+          LPlatformKey := TPath.Combine(LLibraryKey, CWin32Key);
+          FOptions.Add(TDNEnvironmentOptions.Create(cpWin32, LPlatformKey) as IDNEnvironmentOptions);
+          FSupportedPlatforms := FSupportedPlatforms + [cpWin32];
+        end;
+        if LReg.KeyExists(CWin64Key) then
+        begin
+          LPlatformKey := TPath.Combine(LLibraryKey, CWin64Key);
+          FOptions.Add(TDNEnvironmentOptions.Create(cpWin64, LPlatformKey) as IDNEnvironmentOptions);
+          FSupportedPlatforms := FSupportedPlatforms + [cpWin64];
+        end;
+      end;
+    end;
+  finally
+    LReg.Free;
+    LNames.Free;
+  end;
+end;
+
+{ TDNEnvironmentOptions }
+
+procedure TDNEnvironmentOptions.BeginUpdate;
+begin
+  Inc(FUpdateLevel);
+end;
+
+procedure TDNEnvironmentOptions.Changed;
+begin
+  ReloadEnvironmentOptions();
+end;
+
+procedure TDNEnvironmentOptions.Close;
+begin
+  FRegistry.CloseKey();
+end;
+
+constructor TDNEnvironmentOptions.Create(APlatform: TDNCompilerPlatform;
+  const ARegistryKey: string);
+begin
+  inherited Create();
+  FPlatform := APlatform;
+  FRegstryKey := ARegistryKey;
+  FRegistry := TRegistry.Create();
+  FRegistry.RootKey := HKEY_CURRENT_USER;
+end;
+
+destructor TDNEnvironmentOptions.Detsroy;
+begin
+  FRegistry.Free;
+  inherited;
+end;
+
+procedure TDNEnvironmentOptions.EndUpdate;
+begin
+  if FUpdateLevel > 0 then
+  begin
+    Dec(FUpdateLevel);
+    if FUpdateLevel = 0 then
+      Changed();
+  end;
+end;
+
+function TDNEnvironmentOptions.GetBPLOutput: string;
+begin
+  Result := ReadString('Package DPL Output');
+end;
+
+function TDNEnvironmentOptions.GetBrowsingPath: string;
+begin
+  Result := ReadString('Browsing Path');
+end;
+
+function TDNEnvironmentOptions.GetDCPOutput: string;
+begin
+  Result := ReadString('Package DCP Output');
+end;
+
+function TDNEnvironmentOptions.GetPlatform: TDNCompilerPlatform;
+begin
+  Result := FPlatform;
+end;
+
+function TDNEnvironmentOptions.GetSearchPath: string;
+begin
+  Result := ReadString('Search Path');
+end;
+
+function TDNEnvironmentOptions.Open: Boolean;
+begin
+  Result := FRegistry.OpenKey(FRegstryKey, False);
+end;
+
+function TDNEnvironmentOptions.ReadString(const AName: string): string;
+begin
+  if Open then
+  begin
+    try
+      Result := FRegistry.ReadString(AName);
+    finally
+      Close();
+    end;
+  end
+  else
+    Result := '';
+end;
+
+procedure TDNEnvironmentOptions.SetBPLOutput(const Value: string);
+begin
+  WriteString('Package DPL Output', Value);
+end;
+
+procedure TDNEnvironmentOptions.SetBrowsingPath(const Value: string);
+begin
+  WriteString('Browsing Path', Value);
+end;
+
+procedure TDNEnvironmentOptions.SetDCPOutput(const Value: string);
+begin
+  WriteString('Package DCP Output', Value);
+end;
+
+procedure TDNEnvironmentOptions.SetSearchPath(const Value: string);
+begin
+  WriteString('Search Path', Value);
+end;
+
+procedure TDNEnvironmentOptions.WriteString(const AName, AValue: string);
+begin
+  if Open() then
+  begin
+    try
+      FRegistry.WriteString(AName, AValue);
+    finally
+      Close();
+    end;
+    if FUpdateLevel = 0 then
+      Changed();
+  end;
+end;
+
+initialization
+  GDelphinusIDEServices := TDNEnvironmentOptionsService.Create();
+
+finalization
+  GDelphinusIDEServices := nil;
+
+end.
