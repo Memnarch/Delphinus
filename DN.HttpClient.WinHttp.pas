062fd3ffb6608c45fe38c8f93b63d96576d394a0
Memnarch <Sebal007@freenet.de>
2016-01-13 23:52:24 +0100

    recreate WinHttpRequest when threads change, as it is not allowed to use the same request in different threads

--- DN.HttpClient.WinHttp.pas
+++ DN.HttpClient.WinHttp.pas
@@ -4,6 +4,7 @@ interface
 
 uses
   Classes,
+  SysUtils,
   DN.HttpClient,
   DN.Import.WinHttp;
 
@@ -11,6 +12,7 @@ type
   TDNWinHttpClient = class(TDNHttpClient)
   private
     FRequest: IWinHttpRequest;
+    FLastThreadID: Cardinal;
   public
     constructor Create;
     destructor Destroy; override;
@@ -21,6 +23,7 @@ implementation
 
 uses
   ActiveX,
+  Windows,
   DN.HttpClient.Intf;
 
 { TDNWinHttpClient }
@@ -28,7 +31,6 @@ uses
 constructor TDNWinHttpClient.Create;
 begin
   inherited;
-  FRequest := CoWinHttpRequest.Create();
 end;
 
 destructor TDNWinHttpClient.Destroy;
@@ -40,9 +42,15 @@ end;
 function TDNWinHttpClient.Get(const AUrl: string; AResponse: TStream): Integer;
 var
   LResponse: IStream;
-  LAdapter: TStreamAdapter;
+  LAdapter: IStream;
   LRead, LWritten: Int64;
 begin
+
+  if (not Assigned(FRequest)) or (FLastThreadID <> GetCurrentThreadId())  then
+  begin
+    FRequest := CoWinHttpRequest.Create();
+    FLastThreadID := GetCurrentThreadId();
+  end;
   FRequest.Open('Get', AUrl, False);
   FRequest.SetRequestHeader('Authorization', Authentication);
   FRequest.Send('');
@@ -51,11 +59,7 @@ begin
   begin
     LResponse := IUnknown(FRequest.ResponseStream) as IStream;
     LAdapter := TStreamAdapter.Create(AResponse);
-    try
-      LResponse.CopyTo(LAdapter, High(Int64), LRead, LWritten);
-    finally
-      LAdapter.Free;
-    end;
+    LResponse.CopyTo(LAdapter, High(Int64), LRead, LWritten);
   end;
 end;
 
