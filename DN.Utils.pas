e394691e0a9cf12833c68e3c290517d7e3d994fd
PC <Pawe1@users.noreply.github.com>
2019-02-10 23:18:03 +0100

    Updated list of Delphi codenames

--- DN.Utils.pas
+++ DN.Utils.pas
@@ -23,9 +23,9 @@ uses
   SysUtils;
 
 const
-  CDelphiNames: array[9..32] of string =
+  CDelphiNames: array[9..33] of string =
   ('2', '3', '3', '4', '5', '6', '7', '8', '2005', '2006', '2007', '2009', '2010',
-   'XE', 'XE2', 'XE3', 'XE4', 'XE5', 'XE6', 'XE7', 'XE8', 'Seattle', 'Berlin', 'Tokyo');
+   'XE', 'XE2', 'XE3', 'XE4', 'XE5', 'XE6', 'XE7', 'XE8', 'Seattle', 'Berlin', 'Tokyo', 'Rio');
 
 function GetDelphiName(const ACompilerVersion: TCompilerVersion): string;
 var
